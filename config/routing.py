7f67fb4b94ae893d1ae1a3075255b6bd938fa099
Hae Bin <shb0107@gmail.com>
2020-06-05 21:19:16 +0900

    facebook frontend 추가

--- config/routing.py
+++ config/routing.py
@@ -5,7 +5,8 @@ import chat.routing
 # 클라이언트와 Channels 개발 서버가 연결 될 때, 어느 protocol 타입의 연결인지
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
-    # 만약에 websocket protocol 이라면, AuthMiddlewareStack
+    # 만약에 websocket protocol 이라면, AuthMiddlewareStack(현재 인증된 사용자에 대한 참조로 scope를 결정)
+    # cf) Django의 AuthenticationMiddleware에서는 모든 incomming HTTPRequest object에 user attribute를 추가
     'websocket': AuthMiddlewareStack(
         # URLRouter 로 연결, 소비자의 라우트 연결 HTTP path를 조사
         URLRouter(
--- config/settings.py
+++ config/settings.py
@@ -33,12 +33,12 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'channels',
     'chat',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.admin',  # 관리용 사이트
+    'django.contrib.auth',  # 인증 시스템
+    'django.contrib.contenttypes',  # 컨텐츠 타입을 위한 프레임워크
+    'django.contrib.sessions',  # 세션 프레임워크
+    'django.contrib.messages',  # 메세징 프레임워크
+    'django.contrib.staticfiles',  # 정적 파일을 관리하는 프레임워크
     'django.contrib.sites',
     'allauth',
     'allauth.account',
