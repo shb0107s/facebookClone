9bbe3da6ef6db06c6b9370c201d260dbda69ec2e
Hae Bin <shb0107@gmail.com>
2020-06-09 04:23:49 +0900

    frontend 파트 작성 완료

--- facebook_frontend/js/main.js
+++ facebook_frontend/js/main.js
@@ -3,6 +3,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const leftBox = document.querySelector('.left_box')
     const rightBox = document.querySelector('.right_box')
     const feed = document.querySelector('#contents_container')
+    const txt = document.querySelector('#comment37')
 
     leftBox.style.right = `${innerWidth*0.5 + 430}px`
     rightBox.style.left = `${innerWidth*0.5 + 90}px`
@@ -14,12 +15,39 @@ window.addEventListener('DOMContentLoaded', () => {
     function notification(){
         this.classList.toggle('on')
     }
+    function addMorePostAjax(data){
+        feed.insertAdjacentHTML('beforeend', data)
+    }
+    function callMorePostAjax(pageValue){
+        if(pageValue > 5) return
+
+        $.ajax({
+            type: 'POST',
+            url: 'data/post.html',
+            data: pageValue,
+            dataType: 'json',
+            success: addMorePostAjax,
+            error: () => {
+                alert('문제가 발생했습니다')
+            }
+        })
+
+    }
     function scrollFunc() {
         let documentHeight = document.body.scrollHeight
         let scrollHeight = pageYOffset + innerHeight
 
         if(scrollHeight >= documentHeight){
-            // console.log(scrollHeight)
+            let pager = document.querySelector('#page')
+
+            // querySelector는 살아있는 컬렉션을 반환하지 않으므로 다시 찾아서 value를 반환한다.
+            let pageValue = document.querySelector('#page').value
+
+            pager.value = parseInt(pageValue) + 1
+
+            callMorePostAjax(pageValue)
+
+            if(pageValue > 5) return
         }
     }
     function delegation(e){
@@ -57,6 +85,40 @@ window.addEventListener('DOMContentLoaded', () => {
         }else if(elem.matches('[data-name="more"]')){
             // console.log('more!!')
             elem.classList.toggle('active')
+        }else if(elem.matches('[data-name="send"]')){
+            $.ajax({
+                type: 'POST',
+                url: 'data/comment.html',
+                data: '',
+                dataType: 'html',
+                success: (res) => {
+                    document.querySelector('.comment_container').insertAdjacentHTML('beforeend', res)
+                },
+                error: () => {
+                    alert('로그인이 필요합니다.')
+                    window.location.replace('https://www.naver.com')
+                }
+            })
+            // 댓글 작성창 비우기
+            txt.value = ''
+        }else if(elem.matches('[data-name="delete"]')){
+            if(confirm("정말 삭제하시겠습니까?") === true){
+                $.ajax({
+                    type: 'POST',
+                    url: 'data/delete.json',
+                    data: '',
+                    dataType: 'json',
+                    success: (res) => {
+                        if(res.status){
+                            let comt = document.querySelector('.comment-37')
+                            comt.remove()
+                        }
+                    },
+                    error: () => {
+
+                    }
+                })
+            }
         }
     }
 
---
+++ facebook_frontend/js/new_post.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const fileInput = document.querySelector("#id_photo")
+    const submit = document.querySelector('#submitBtn')
+    const canvas = document.querySelector('#imageCanvas')
+    let ctx = canvas.getContext('2d')
+
+    function handleImage(){
+        let fileList = fileInput.files
+        let reader = new FileReader()
+
+        submit.disabled = false
+
+        reader.readAsDataURL(fileList[0])
+        reader.onload = event => {
+            let img = new Image()
+            img.onload = () => {
+                canvas.width = 100
+                canvas.height = 100
+                ctx.drawImage(img, 0, 0, 100, 100)
+
+                submit.parentNode.style.display = 'block'
+            }
+
+            // 이미지 로딩 지시. 이미지 로딩이 완료될때 onload 호출
+            img.src = event.target.result
+        }
+    }
+
+    fileInput.addEventListener('change', handleImage)
+})
